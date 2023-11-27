a43c3485ba060f418812864884c79f1b27a11a49
linde <paul.linden@gritacademy.se>
2023-11-27 14:12:39 +0100

    ..

--- src/test/java/org/example/PrimeTest.java
+++ src/test/java/org/example/PrimeTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -8,88 +10,105 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testing Prime class")
 class PrimeTest {
 
     Prime prime;
-    //Test for correct init values
+
+    @AfterEach
+    @DisplayName("Test succeeded")
+    public void testSucceed(){
+        System.out.println("Test succeeded");
+    }
+
     @Test
+    @DisplayName("Testing initial value of number of Primes")
     public void testInitValueOfNumberOfPrimes(){
         prime = new Prime();
         assertEquals(0, prime.getNumberOfPrimes());
     }
     @Test
+    @DisplayName("Testing initial value of the sum of Primes")
     public void testInitValueOfSum(){
         prime = new Prime();
         assertEquals(0, prime.getSumOfPrimes());
     }
     @Test
-    public void testInitSizeOfList(){
+    @DisplayName("Testing initial value of list")
+    public void testInitList(){
         prime = new Prime();
         assertNull(prime.getPrimes());
     }
-    //Testing Prime Values
+
+
     @Test
+    @DisplayName("Testing that correct amount of Primes are added")
     public void testNumberOfPrimes(){
         prime = new Prime(0,10);
         assertEquals(4, prime.getNumberOfPrimes());
     }
     @Test
+    @DisplayName("Testing that correct Primes are added to list")
     public void testPrimeList(){
         prime = new Prime(0,10);
         assertIterableEquals(List.of(2, 3, 5, 7),  prime.getPrimes());
     }
     @Test
+    @DisplayName("Testing that sum of primes are correct")
     public void testSumOfPrimes(){
         prime = new Prime(0,10);
         assertEquals(17, prime.getSumOfPrimes());
     }
     @Test
+    @DisplayName("Testing that the maximal numbers of primes are correct")
     public void testMaxNumberOfPrimes(){
         prime = new Prime(0,1000);
         assertEquals(168, prime.getNumberOfPrimes());
     }
     @Test
+    @DisplayName("Testing that the maximal sum of primes are correct")
     public void testMaxValueOfPrimes(){
         prime = new Prime(0,1000);
         assertEquals(76127, prime.getSumOfPrimes());
     }
 
-    //Test of constructor parameters
+
     @Test
+    @DisplayName("Testing that the lowest start value and highest end value in constructor are correct")
     public void testConstructorBoundaries(){
         prime = new Prime(0,1000);
         assertTrue(prime.isValidNumbers());
     }
     @Test
+    @DisplayName("Testing that constructor parameters both are zero")
     public void testConstructorIsZero(){
         prime = new Prime(0,0);
         assertTrue(prime.isValidNumbers());
     }
     @Test
+    @DisplayName("Testing that constructor parameters don't take negative number")
     public void testConstructorFirstNumberIsNegative(){
-        prime = new Prime(-1,1000);
+        prime = new Prime(-1,-1);
         assertFalse(prime.isValidNumbers());
     }
-    @Test
-    public void testConstructorLastNumberIsNegative(){
-        prime = new Prime(-1,1000);
-        assertFalse(prime.isValidNumbers());
-    }
-    @Test
-    public void testConstructorNumberIsTooHigh(){
-        prime = new Prime(0,1001);
-        assertFalse(prime.isValidNumbers());
 
+    @Test
+    @DisplayName("Testing that constructor parameter dont take to high numbers")
+    public void testConstructorNumberIsTooHigh(){
         prime = new Prime(1001,1001);
         assertFalse(prime.isValidNumbers());
     }
+
     @Test
+    @DisplayName("Testing that start value cant be bigger then end value in constructor")
     public void testConstructorNumberIsTooLow(){
         prime = new Prime(11,10);
         assertFalse(prime.isValidNumbers());
     }
-    //Test print methods
+
+
     @Test
+    @DisplayName("Testing that print message of sum is correct")
     public void testPrintSum() {
         prime = new Prime(0, 1000);
 
@@ -104,6 +123,7 @@ class PrimeTest {
         System.setOut(System.out);
     }
     @Test
+    @DisplayName("Testing that print message of sum is correct")
     public void testPrintCount() {
         prime = new Prime(0, 1000);
 
@@ -118,6 +138,7 @@ class PrimeTest {
         System.setOut(System.out);
     }
     @Test
+    @DisplayName("Testing that print message of wrong message is correct")
     public void testWrongMessage() {
         prime = new Prime();
 
